8415d65c26923b52c0f59ceddd2aff05f1fb8881
FX2One <dfoxik@gmail.com>
2022-01-13 09:15:15 +0100

    comic series models added

--- comicstore/comicapp/admin.py
+++ comicstore/comicapp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Comic
 # Register your models here.
-admin.site.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_filter = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Comic, ComicsAdmin)
--- comicstore/comicapp/models.py
+++ comicstore/comicapp/models.py
@@ -6,3 +6,15 @@ class Comic(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return f'{self.title} - {self.slug}'
+
+class ComicSeries(models.Model):
+    series = models.CharField(max_length=200)
+    publishing = models.CharField(max_length=300)
+    episodes = models.IntegerField()
+
+    def __str__(self):
+        return f'series: {self.series}, publishing: {self.publishing}'
+
