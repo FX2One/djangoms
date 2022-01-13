7e929de8617648c19d3165f8c8f18a11f190760a
FX2One <dfoxik@gmail.com>
2022-01-13 15:00:52 +0100

    ComicSeries Model update, Available model for mail sign ups

--- comicstore/comicapp/admin.py
+++ comicstore/comicapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Comic
+from .models import Comic, ComicSeries
 # Register your models here.
+
 class ComicsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug')
-    list_filter = ('title', 'slug')
+    list_display = ('title','series')
+    list_filter = ('title',)
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Comic, ComicsAdmin)
+admin.site.register(ComicSeries)
--- comicstore/comicapp/models.py
+++ comicstore/comicapp/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class ComicSeries(models.Model):
+    series = models.CharField(max_length=200, default='SERIES')
+    publishing = models.CharField(max_length=300)
+    episodes = models.IntegerField()
+
+    def __str__(self):
+        return f'series: {self.series}'
+
+class Available(models.Model):
+    email = models.EmailField(unique=True, max_length=200)
+
+    def __str__(self):
+        return self.email
+
 class Comic(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
+    series = models.ForeignKey(ComicSeries, on_delete=models.CASCADE)
+    subscriber = models.ManyToManyField(Available, blank=True, null=True)
+    
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
-
-class ComicSeries(models.Model):
-    series = models.CharField(max_length=200)
-    publishing = models.CharField(max_length=300)
-    episodes = models.IntegerField()
-
-    def __str__(self):
-        return f'series: {self.series}, publishing: {self.publishing}'
-
